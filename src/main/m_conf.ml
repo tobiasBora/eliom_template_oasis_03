b577ce497e1cc1a7c11ebbf4b5d7491f9b1795dc
Tobias Bora <tobias.bora@gmail.com>
2014-06-02 16:14:46 +0200

    First commit : JS doesn't work

---
+++ src/main/m_conf.ml
@@ -0,0 +1,12 @@
+(*
+ This file is used to configure all data available for each program.
+ The extension .ab will be droped before compilation, and datas comes
+ from the file _oasis.
+
+ *)
+
+let title = "%%%TITLE%%%"
+let description = "des"
+let author = "me"
+let licence = " GPL"
+let version = " 1.0"
